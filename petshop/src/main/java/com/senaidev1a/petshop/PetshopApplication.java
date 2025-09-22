1b50ac1f05399d38124a850b123f852980577f11
Renan Hiramatsu <renanhiramatsu1a@gmail.com>
2025-09-22 15:19:57 -0300

    Add files via upload

---
+++ petshop/src/main/java/com/senaidev1a/petshop/PetshopApplication.java
@@ -0,0 +1,26 @@
+package com.senaidev1a.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopApplication.class, args);
+	}
+		public static void main(String[]args) {
+			SpringApplication.run(PetshopApplication.class, args);
+		}
+		@RestController
+		static class HelloWorldController{
+			
+			//Localhost:8080/ -->Acessar essa função
+			@GetMapping("/")
+			public String helloWorld() {
+				return "Hello World!";
+		}
+	}
+}
